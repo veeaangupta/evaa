a53c81ddee7f34861ceefe032e71ce087de034f2
veeaangupta <95080000+veeaangupta@users.noreply.github.com>
2021-11-26 16:43:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function hbd(){
+    document.getElementById("wish").play()
+}
+function hbdsong(){
+    document.getElementById("song").play()
+}
+function wish()
+{
+    document.getElementById("card").style.display="block"
+  
+    
+}
+
+
+
+
+
